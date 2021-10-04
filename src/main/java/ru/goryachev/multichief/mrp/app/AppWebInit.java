6ae39b4b97ec1b1c20d277be3ebf956f64e898ea
LevGoryachev <l.goryachev@inbox.ru>
2021-10-04 06:48:30 +0300

    Libs: hibernate-core 5.5.0, PostgreSQL Driver, Spring JPA

--- src/main/java/ru/goryachev/multichief/mrp/app/AppWebInit.java
+++ src/main/java/ru/goryachev/multichief/mrp/app/AppWebInit.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "ru.goryachev.multichief.mrp")
+@EnableJpaRepositories(basePackages = "ru.goryachev.multichief")
 @EntityScan(basePackages = "ru.goryachev.multichief.mrp")
 public class AppWebInit extends SpringBootServletInitializer {
 
